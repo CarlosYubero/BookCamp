75cb52e23489f5122509b03b5d6f70b1ffc4d44c
CarlosYubero <carlosyubero91@gmail.com>
2014-11-26 19:23:56 +0100

    Carlos1

--- BookCamp/src/Pantallas/Pantalla1.java
+++ BookCamp/src/Pantallas/Pantalla1.java
@@ -1,13 +1,8 @@
+package Pantallas;
+
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
-import javax.swing.JMenuBar;
-import javax.swing.JPanel;
-import java.awt.BorderLayout;
-import javax.swing.JLabel;
-import javax.swing.ImageIcon;
-import javax.swing.JMenu;
-
 
 public class Pantalla1 {
 
@@ -41,24 +36,8 @@ public class Pantalla1 {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 1094, 745);
+		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		JPanel panel = new JPanel();
-		frame.getContentPane().add(panel, BorderLayout.NORTH);
-		panel.setLayout(new BorderLayout(0, 0));
-		
-		JMenuBar menuBar = new JMenuBar();
-		panel.add(menuBar);
-		
-		JMenu mnFile = new JMenu("file");
-		menuBar.add(mnFile);
-		
-		JLabel label = new JLabel("");
-		label.setIcon(new ImageIcon(Pantalla1.class.getResource("/Imagenes/Libros.jpg")));
-		panel.add(label, BorderLayout.NORTH);
-
-		
 	}
 
 }
