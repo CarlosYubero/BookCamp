0869a747efb36591624dbbcbaaac9f7dbc2f89c8
CarlosYubero <carlosyubero91@gmail.com>
2014-11-28 17:18:55 +0100

    0.1

---
+++ BookCamp/src/Pantallas/Main.java
@@ -0,0 +1,22 @@
+package Pantallas;
+public class Main {
+
+	public static void main(String[] args) {
+		
+	BookStore books = new BookStore("archivo.bin", "Librosl.csv");
+	//books.loadCSVFile();
+	books.addBook(new Book(1500, "Prueba", 1, 2, 2000, 3));
+	//System.out.println(books.readBookById(1).toString());
+	
+//	System.out.println(books.addBook(new Book(1000, "el senor de los anillos", 1,2,3,4)));
+//	books.removeBookById(50);
+	//System.out.println(books.existsBookById(50));
+	//books.printBiblio();
+	//System.out.println(books.readBookById(1).toString());
+	//books.getbooksbyAuthor(1);
+	System.out.println(books.existsBookById(1500));
+	System.out.println(books.readBookById(1500).toString());
+	System.out.println(books.existsBookById(1200));
+	}
+
+}
